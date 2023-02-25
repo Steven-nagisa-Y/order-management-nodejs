9baa5e3c9884420e795e4c62fb56a8493c6ba066
Steven Yan <steven_nagisa@qq.com>
2023-02-25 21:00:25 +0800

    😮新增跨域控制

--- app.js
+++ app.js
@@ -18,19 +18,22 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//设置跨域访问
+const cors = require('cors');
+app.use(cors());
+
 // 日志中间件
 app.use((req, res, next) => {
   console.log(req.method, req.originalUrl, req.query, req.body);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.json({ session: req.session, cookie: req.cookies });
+app.get("/", authenticateToken, (req, res) => {
+  res.json({ ok: true});
 });
 
 app.use("/auth", admin);
 
-// 暂时不开启auth
 // 客户
 app.use("/customers", authenticateToken, customers);
 // 订单
@@ -40,16 +43,12 @@ app.use("/product", authenticateToken, product);
 // 订单数量
 app.use("/quantity", authenticateToken, quantity);
 
-// 禁止未定义的请求
-app.use((req, res, next) => {
-  res.status(405).send("Method Not Allowed");
-});
-
+// 屏蔽未设置的url
 app.use("*", (req, res, next) => {
   res.status(404).send("Page Not Found");
 });
 
 // 启动服务器
 app.listen(Port, () => {
-  console.log("Server started on port 3000");
+  console.log("Server started on port " + Port);
 });
